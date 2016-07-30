5408d998c147bc41ff1d11cd8ccc72f336c64ea4
dohtml5 <dohtml5@sohu.com>
2016-07-30 16:38:49 +0800

    wbc6

--- js/js/helper.js
+++ js/js/helper.js
@@ -1,52 +1,78 @@
-var Helper = {};
+!function(window, document, undefined) {
 
-Helper.getType = function(item) {
-	if (typeof item == 'object') {
-		if (item instanceof Array) {
-			return "array";
+	var Helper = {};
+
+	Helper.deepCopy = function(list) {
+		var result;
+
+		if (typeof list != 'object') {
+			return;
 		}
-		return "object";
-	} else {
-		return typeof item;
-	}
-}
 
-Helper.getDateArr = function(date) {
-	var y = date.getFullYear();
-	var M = date.getMonth() + 1;
-	var d = date.getDate();
+		if (list instanceof Array) {
+			result = [];
+		} else {
+			result = {};
+		}
 
-	var h = date.getHours();
-	var m = date.getMinutes();
-	var s = date.getSeconds();
+		for (var key in list) {
+			result[key] = list[key];
+		}
 
-	return [y, M, d, h, m, s];
-}
-
-Helper.dom = function(id) {
-	if (typeof id == 'string') {
-		return document.getElementById(id);
-	} else {
-		return id;
-	}
-}
-
-Helper.fill0 = function(num) {
-	return num < 10 ? '0' + num : num;
-}
-
-Helper.isStartWith = function(str, char) {
-
-	if (str.charAt(0) == char) {
-		return true;
+		return result;
 	}
 
-	return false;
-
-	/*if (str.indexOf(char) == 0) {
-		return true;
+	Helper.getType = function(item) {
+		if (typeof item == 'object') {
+			if (item instanceof Array) {
+				return "array";
+			}
+			return "object";
+		} else {
+			return typeof item;
+		}
 	}
 
-	return false;*/
+	Helper.getDateArr = function(date) {
+		var y = date.getFullYear();
+		var M = date.getMonth() + 1;
+		var d = date.getDate();
 
-}
+		var h = date.getHours();
+		var m = date.getMinutes();
+		var s = date.getSeconds();
+
+		return [y, M, d, h, m, s];
+	}
+
+	Helper.dom = function(id) {
+		if (typeof id == 'string') {
+			return document.getElementById(id);
+		} else {
+			return id;
+		}
+	}
+
+	Helper.fill0 = function(num) {
+		return num < 10 ? '0' + num : num;
+	}
+
+	Helper.isStartWith = function(str, char) {
+
+		if (str.charAt(0) == char) {
+			return true;
+		}
+
+		return false;
+
+		/*if (str.indexOf(char) == 0) {
+			return true;
+		}
+
+		return false;*/
+
+	}
+
+	window.Helper = Helper;
+
+}(window, document);
