0c92ef1fa4bc8f36f18fddd7367bd52f4ebe0f02
dohtml5 <dohtml5@sohu.com>
2016-08-28 16:43:00 +0800

    angularjs图片上传

--- api/books_add.php
+++ api/books_add.php
@@ -12,6 +12,7 @@
     $classify = $_GET["classify"];
     $status = $_GET["status"];
 	$borrow_status = $_GET["borrow_status"];
+	$bookPic = $_GET["bookPic"];
 
 	// $now = date("Y-m-d h:i:s");
 
@@ -23,7 +24,8 @@
         "price" => $price,
         "classify" => $classify,
         "status" => $status,
-        "borrow_status" => $borrow_status
+        "borrow_status" => $borrow_status,
+        "bookPic" => $bookPic
     );
 
     $id = $db->insert ('books', $data);
--- libraryMng/lib/js/library-ng.js
+++ libraryMng/lib/js/library-ng.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('libraryApp', []);
 
-app.controller('mainController', function($scope, $http) {
+app.controller('mainController', function($scope, $http, $timeout) {
 
     $scope.params = {
         ids: []
@@ -12,7 +12,13 @@ app.controller('mainController', function($scope, $http) {
 
     var getBookList = function() {
 
-        layer.load();
+        $timeout(function() {
+            layer.load();
+        });
+
+        /*setTimeout(function() {
+            layer.load();
+        }, 0);*/
 
         $http({
             url: '../api/books_list.php',
@@ -47,8 +53,6 @@ app.controller('mainController', function($scope, $http) {
             }
         }
 
-        console.log($scope.params.ids);
-
     };
 
     function getIndex(arr, item) {
@@ -63,4 +67,120 @@ app.controller('mainController', function($scope, $http) {
         return -1;
     }
 
+    /////////////////////////////////////////////////
+
+    /**
+     * 根据小图显示大图
+     */
+    $scope.showPic = function() {
+
+        var url = this.book.bookPic;
+
+        layer.open({
+            type: 1,
+            title: false,
+            closeBtn: 0,
+            area: '800px',
+            skin: 'layui-layer-nobg', //没有背景色
+            shadeClose: true,
+            content: '<img src="../fileUploading/server/uploadImgs/'+url+'">'
+        });
+
+    };
+
+    /**
+     * 新增图书
+     */
+    $scope.addBook = function() {
+
+        layer.open({
+            type: 1,
+            shade: false,
+            title: '新增图书',
+            content: $('.book-form'),
+            btn: ['确定', '取消'],
+            area: ['400px', '190px'],
+            yes: function(index) {
+
+                // 表单验证
+
+                if (!$scope.bookPicUrl) {
+                    alert('请上传图书封面');
+                    return;
+                }
+
+                var data = {
+                    name: $scope.book.name,
+                    bookPic: $scope.bookPicUrl,
+                    author: 'author1',
+                    publisher: 'test',
+                    price: 22.5,
+                    p_date: '2016-09-21',
+                    classify: 'classify',
+                    status: 1,
+                    borrow_status: 0
+                };
+
+                var url = '../api/books_add.php';
+
+                $http({
+                    url: url,
+                    method: 'get',
+                    params: data
+                }).success(function(response) {
+                    getBookList();
+                    $scope.bookPicUrl = null;
+                    layer.close(index);
+                });
+
+                console.log(data)
+            }
+        });
+    };
+
+    /**
+     * 上传图书封面
+     */
+    $scope.savePic = function() {
+
+        var fd = new FormData();
+        var file = document.querySelector('input[type=file]').files[0];
+        fd.append('pic', file);
+
+        $http({
+            method: 'post',
+            url: '../fileUploading/server/step1.php',
+            data: fd,
+            headers: {'Content-Type': undefined},
+            transformRequest: angular.identity
+        }).success(function(response) {
+            if (response.success) {
+                $scope.bookPicUrl = response.fileName;
+            }
+        });
+    }
+
 });
+
+//app.controller('uploadController', function($scope, $http) {
+//
+//    $scope.save = function() {
+//
+//        var fd = new FormData();
+//        var file = document.querySelector('input[type=file]').files[0];
+//        fd.append('pic', file);
+//
+//        $http({
+//            method: 'post',
+//            url: '../fileUploading/server/step1.php',
+//            data: fd,
+//            headers: {'Content-Type':undefined},
+//            transformRequest: angular.identity
+//        }).success(function(response) {
+//            if (response.success) {
+//
+//            }
+//        });
+//    }
+//
+//});
